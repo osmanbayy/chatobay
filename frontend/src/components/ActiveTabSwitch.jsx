72046ac72cd329bdaa85cae51a0d48f616e79a44
Osman Bay <bayosman342@gmail.com>
2025-11-08 18:15:08 +0300

    feat: sidebar completed
    --enhance chat application with loading states, profile updates, and tab navigation

--- frontend/src/components/ActiveTabSwitch.jsx
+++ frontend/src/components/ActiveTabSwitch.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useChatStore } from "../store/useChatStore";
 
 const ActiveTabSwitch = () => {
+  const { activeTab, setActiveTab } = useChatStore();
   return (
-    <div>
-      ActiveTabSwitch
+    <div className="p-2 m-2 bg-transparent tabs tabs-boxed">
+      <button
+        className={`tab ${
+          activeTab === "chats"
+            ? "bg-cyan-500/20 text-cyan-400"
+            : "text-slate-400"
+        }`}
+        onClick={() => setActiveTab("chats")}
+      >
+        Chats
+      </button>
+      <button
+        className={`tab ${
+          activeTab === "contacts"
+            ? "bg-cyan-500/20 text-cyan-400"
+            : "text-slate-400"
+        }`}
+        onClick={() => setActiveTab("contacts")}
+      >
+        Contacts
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default ActiveTabSwitch
+export default ActiveTabSwitch;
--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -1,11 +1,113 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { useChatStore } from "../store/useChatStore";
+import { LogOutIcon, Volume2Icon, VolumeOffIcon } from "lucide-react";
+
+const mouseClickSound = new Audio("/sounds/mouse-click.mp3");
 
 const ProfileHeader = () => {
-  return (
-    <div>
-      ProfileHeader
-    </div>
-  )
-}
+  const { logout, updateProfile, authUser, isUpdatingProfile } = useAuthStore();
+  const { isSoundEnabled, toggleSound } = useChatStore();
+  const [selectedImage, setSelectedImage] = useState(null);
 
-export default ProfileHeader
+  const fileInputRef = useRef();
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = async () => {
+      const base64Image = reader.result;
+      setSelectedImage(base64Image);
+      await updateProfile({ profilePic: base64Image });
+    };
+  };
+
+  return (
+    <div className="p-6 border-b border-slate-700/50">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          {/* Avatar */}
+          <div className="avatar online">
+            <button
+              onClick={() => fileInputRef.current.click()}
+              className="relative overflow-hidden rounded-full size-14 group"
+            >
+              {/* Profile image */}
+              <img
+                src={selectedImage || authUser.profilePic || "/avatar.png"}
+                alt="User image"
+                className={`object-cover size-full transition-opacity ${
+                  isUpdatingProfile ? "opacity-50 blur-[1px]" : ""
+                }`}
+              />
+
+              {/* Hover Change text */}
+              <div className="absolute inset-0 flex items-center justify-center transition-opacity opacity-0 bg-black/50 group-hover:opacity-100">
+                <span className="text-xs text-white">Change</span>
+              </div>
+
+              {/* Loading Spinner */}
+              {isUpdatingProfile && (
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <div className="w-6 h-6 border-2 border-white rounded-full border-t-transparent animate-spin"></div>
+                </div>
+              )}
+            </button>
+
+            <input
+              type="file"
+              accept="image/*"
+              ref={fileInputRef}
+              onChange={handleImageUpload}
+              className="hidden"
+            />
+          </div>
+
+          {/* Username & online text */}
+          <div>
+            <h3 className="text-base font-medium text-slate-200 max-w-[180px] truncate">
+              {authUser.fullName}
+            </h3>
+            <p className="text-xs text-slate-400">Online</p>
+          </div>
+        </div>
+
+        {/* Buttons */}
+        <div className="flex items-center gap-4">
+          {/* Logout Button */}
+          <button
+            onClick={logout}
+            className="transition-colors text-slate-400 hover:text-slate-200"
+          >
+            <LogOutIcon className="size-5" />
+          </button>
+
+          {/* Sound Toggle Button */}
+          <button
+            className="transition-colors text-slate-400 hover:text-slate-200"
+            onClick={() => {
+              // play click sound before toggling
+              mouseClickSound.currentTime = 0; // reset to start
+              mouseClickSound
+                .play()
+                .catch((error) => console.log("Audio play failed:", error));
+              toggleSound();
+            }}
+          >
+            {isSoundEnabled ? (
+              <Volume2Icon className="size-5" />
+            ) : (
+              <VolumeOffIcon className="size-5" />
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileHeader;
